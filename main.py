0ced36d102a31ee87da2833c2e84135ec1265ae9
Meikaijie <christophermei@Christophers-MacBook-Pro.local>
2017-12-27 02:36:07 -0500

    clean up and slight algorithm changes

--- main.py
+++ main.py
@@ -1,24 +1,56 @@
-import numpy as np
-from convexPolygon import Simplex
+import argparse
 import util
 
-def main():
-	# Single Test
-	packedTets = util.randomizedGuidedPacking(4, genmult=2, geninc=2, initstepscale=0.2, stepscalereduction=0.95, rotreduction=0.95, max_iters=1000)
-	util.plot_tetrahedra(packedTets, 'test_tets4_is2_ir360_sr95_rr95_iter1000')
+def parse_args():
+	parser = argparse.ArgumentParser(description="Tetrahedron Packing Simulation")
+	parser.add_argument("-n", "--numtetras", type=int, help="number of tetrahedra to pack, valid at or above 1", required=True)
+	parser.add_argument("-is", "--initstepscale", type=float, help="initial step size coefficient, valid between 0 and 1", default=0.9)
+	parser.add_argument("-sr", "--stepscalereduction", type=float, help="step scale decay rate, valid between 0 and 1", default=0.999)
+	parser.add_argument("-st", "--stepthreshold", type=float, help="minimum allowed step scale, valid above 0", default=1e-6)
+	parser.add_argument("-ir", "--initrotationrange", type=float, help="initial permitted range of a single rotation move", default=360.0)
+	parser.add_argument("-rr", "--rotreduction", type=float, help="rotation range decay rate, valid between 0 and 1", default=0.999)
+	parser.add_argument("-rt", "--rotationthreshold", type=float, help="minimum allowed rotation range", default=0.1)
+	parser.add_argument("-l", "--length", type=float, help="edge length of the tetrahedra", default=1.0)
+	parser.add_argument("-i", "--iterations", type=int, help="max number of iterations", default=5000)
+	parser.add_argument("-v", "--verbose", action='store_true')
+	parser.add_argument("-b", "--bias", help="induce pairwise attraction bias between tetrahedra to avoid dogpile clustering", action='store_true')
+	parser.add_argument("-np", "--noplot", help="disable plotting tetrahedra with plotly", action='store_true', default=False)
+	return parser.parse_args()
 
-	# Aggregate statistic test
-	# bestTets = None
-	# bestDensity = 0
-	# densities = []
-	# for _ in range(100):
-	# 	packedTets = util.randomizedGuidedPacking(2, genmult=2, geninc=2, initstepscale=0.9, stepscalereduction=0.95, rotreduction=0.95, max_iters=1000)
-	# 	density = 1.0/(6*2**0.5)*2/util.cubeContainerVolume(packedTets)
-	# 	densities.append(density)
-	# 	if density > bestDensity:
-	# 		bestTets = packedTets
-	# util.plot_tetrahedra(bestTets, 'test_tets2_is2_ir360_sr95_rr95_iter1000')
-	# print(sorted(densities))
+def main():
+	args = parse_args()
+	packedTets = None
+	filename = "graph_n"+str(args.numtetras)+"_is"+str(args.initstepscale)+"_sr"+str(args.stepscalereduction)+"_st"+str(args.stepthreshold)+"_ir"+str(args.initrotationrange)+"_rr"+str(args.rotreduction)+"_rt"+str(args.rotationthreshold)+"_l"+str(args.length)+"_i"+str(args.iterations)
+	if args.bias:
+		filename += "_withbias"
+		packedTets = util.randomizedGuidedPackingV3(args.numtetras, initstepscale=args.initstepscale, stepscalereduction=args.stepscalereduction,
+					initrotrange=args.initrotationrange, rotreduction=args.rotreduction, stepthreshold=args.stepthreshold,
+					rotationthreshold=args.rotationthreshold, l=args.length, max_iters=args.iterations, verbose=args.verbose)
+	else:
+		packedTets = util.randomizedGuidedPackingV2(args.numtetras, initstepscale=args.initstepscale, stepscalereduction=args.stepscalereduction,
+					initrotrange=args.initrotationrange, rotreduction=args.rotreduction, stepthreshold=args.stepthreshold,
+					rotationthreshold=args.rotationthreshold, l=args.length, max_iters=args.iterations, verbose=args.verbose)
+	output = ""
+	with open("packing_output.txt","wb") as f:
+		for basis in packedTets[0].b:
+			line = ""
+			for coord in basis:
+				line += str(coord)+" "
+			line += "\n"
+			f.write(line)
+			output += line
+		for tet in packedTets:
+			line = ""
+			for vertex in tet.v:
+				for coord in vertex:
+					line += str(coord)+" "
+			line += "\n"
+			f.write(line)
+			output += line
+	print(output)
+	if not args.noplot:
+		util.plot_tetrahedra(packedTets, filename+".html")
+
 
 if __name__ == "__main__":
 	main()
--- util.py
+++ util.py
@@ -27,7 +27,6 @@ def plot_tetrahedra(tetrahedra_list, filename):
 # Takes in 2 3-D simplex objects and a subprocess running a clojure REPL
 # Returns True if they do, False otherwise
 def collision_detection(tetra1, tetra2, subproc):
-	# start = time.time()
 	l = np.linalg.norm(tetra1.v[0]-tetra1.c)
 	# If the centroids of 2 tetrahedra are not at least this close
 	# then there is no need to perform the expensive check
@@ -63,6 +62,8 @@ def collision_detection(tetra1, tetra2, subproc):
 		return collided == 'true\n'
 
 # Get volume of smallest axis aligned rectangular prism containing all tetrahedra
+# Used to observe convergence; not a good measure of packing quality because
+# the box is axis aligned meaning the volume is greatly affected by tetrahedra orientations
 def cubeContainerVolume(tetlist):
 	corners = [[None,None] for _ in range(len(tetlist[0].c))]
 	for tet in tetlist:
@@ -107,7 +108,7 @@ def generate_tetrahedra(numtetras, initialmultiplier=2, multiplierincrement=2, l
 	return tetralist
 
 # Get "center of mass" of all tetrahedra,
-# calculated as the average of all tetrahedra centroids
+# calculated as the average of all tetrahedra centroid positions
 def getCOM(tetralist):
 	COM = np.zeros(len(tetralist[0].c))
 	for tet in tetralist:
@@ -118,19 +119,25 @@ def getCOM(tetralist):
 # Packing algorithm that randomly initializes numtetras tetrahedra in space
 # then compresses them by performing random translations and rotations that 
 # move the tetrahedra towards the collective "center of mass"
-def randomizedGuidedPacking(numtetras, genmult=2, geninc=2, initstepscale=0.9, stepscalereduction=0.95, initrotrange=360, rotreduction=0.95, stepthreshold=1e-6, rotationthreshold=0.1, l=1.0, max_iters=1000):
+def randomizedGuidedPacking(numtetras, genmult=2, geninc=2, initstepscale=0.9, stepscalereduction=0.95, initrotrange=360, rotreduction=0.95, stepthreshold=1e-6, rotationthreshold=0.1, l=1.0, max_iters=1000, verbose=False):
+	assert(initstepscale>0 and initstepscale<=1)
+	assert(stepscalereduction>0 and stepscalereduction<1)
+	assert(stepthreshold>0)
+	assert(rotreduction>0 and rotreduction<1)
+
+	# Can be removed if python implementation of collision detection is used
 	subproc = subprocess.Popen('clojure', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 	subproc.stdin.write('(clojure.main/load-script "tetrahedron-intersect.clj")')
 	subproc.stdin.write('(flush)')
 	subproc.stdout.readline() # flush out useless line
 	subproc.stdout.readline() # flush out useless line
+
 	tetralist = generate_tetrahedra(numtetras,genmult,geninc,l)
 	scales = [initstepscale]*len(tetralist)
 	rotranges = [initrotrange]*len(tetralist)
 	converged = [False]*len(tetralist)
 	centerOfMass = getCOM(tetralist)
 	for iteration in range(max_iters):
-		print("Iteration: " + str(iteration))
 		updatedlist = []
 		for i, tet in enumerate(tetralist):
 			# preliminary convergence check
@@ -217,21 +224,314 @@ def randomizedGuidedPacking(numtetras, genmult=2, geninc=2, initstepscale=0.9, s
 
 		# update tetralist, print density calculation, check convergence condition
 		tetralist = updatedlist[:]
-		if iteration % 10 == 9:
+		if verbose and iteration % 10 == 9:
+			print("Iteration: " + str(iteration+1))
 			cubeVolume = cubeContainerVolume(tetralist)
-			print "container volume: ", cubeVolume
+			print "box container volume: ", cubeVolume
 			tetVolume = l**3.0/(6*2**0.5)*numtetras
 			print "density: ", tetVolume/cubeVolume
 		if not False in converged:
 			break
 	return tetralist
 
-# def dimerPacking():
+# Packing algorithm that randomly initializes numtetras tetrahedra in space
+# then compresses them by performing random translations and rotations that 
+# move the tetrahedra towards the collective "center of mass"
+# V2 adds more dynamic rotations
+def randomizedGuidedPackingV2(numtetras, genmult=2, geninc=2, initstepscale=0.9, stepscalereduction=0.95, initrotrange=360, rotreduction=0.95, stepthreshold=1e-6, rotationthreshold=0.1, l=1.0, max_iters=1000, verbose=False):
+	assert(initstepscale>0 and initstepscale<=1)
+	assert(stepscalereduction>0 and stepscalereduction<1)
+	assert(stepthreshold>0)
+	assert(rotreduction>0 and rotreduction<1)
 
+	# Can be removed if python implementation of collision detection is used
+	subproc = subprocess.Popen('clojure', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	subproc.stdin.write('(clojure.main/load-script "tetrahedron-intersect.clj")')
+	subproc.stdin.write('(flush)')
+	subproc.stdout.readline() # flush out useless line
+	subproc.stdout.readline() # flush out useless line
 
+	tetralist = generate_tetrahedra(numtetras,genmult,geninc,l)
+	scales = [initstepscale]*len(tetralist)
+	rotranges = [initrotrange]*len(tetralist)
+	converged = [False]*len(tetralist)
+	centerOfMass = getCOM(tetralist)
+	for iteration in range(max_iters):
+		updatedlist = []
+		for i, tet in enumerate(tetralist):
+			# preliminary convergence check
+			useTranslation = True
+			useRotation = True
+			if scales[i] < stepthreshold and rotranges[i] < rotationthreshold:
+				updatedlist.append(tet)
+				converged[i] = True
+				continue
 
+			# get translation
+			dist = np.linalg.norm(centerOfMass-tet.c)
+			step_size = scales[i]*dist
+			tvec = np.random.uniform(-step_size,step_size,len(tet.c))
+			# flip diretion of translation if it moves the tetrahedron away from COM
+			if np.linalg.norm(centerOfMass-tet.c-tvec)>dist:
+				tvec *= -1
+			translatedTet = Simplex(tet.v)
+			translatedTet.translate(tvec)
+			# reject translation if it results in a collision and scale back step size
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					if collision_detection(translatedTet,updatedlist[j],subproc):
+						useTranslation = False
+						scales[i] *= stepscalereduction
+						break
+				else:
+					if collision_detection(translatedTet,tetralist[j],subproc):
+						useTranslation = False
+						scales[i] *= stepscalereduction
+						break
 
+			tetUsed = tet
+			if useTranslation:
+				tetUsed = translatedTet
 
+			# get rotation
+			degreeslist = []
+			planelist = []
+			for j in range(len(tet.c)):
+				for k in range(j+1,len(tet.c)):
+					degrees = np.random.uniform(rotranges[i])
+					plane = (j,k)
+					rotatedTet = Simplex(tetUsed.v)
+					rotatedTet.Rotate(degrees,plane,'deg')
+					netdist = 0
+					netrdist = 0
+					for n in range(len(tetUsed.v)):
+						d = np.linalg.norm(centerOfMass-tetUsed.v[n])
+						rd = np.linalg.norm(centerOfMass-rotatedTet.v[n])
+						netdist += d
+						netrdist += rd
+					# flip direction of rotation if it increases net vertex distance from COM
+					if netrdist > netdist:
+						degrees *= -1
+					degreeslist.append(degrees)
+					planelist.append(plane)
+			rotatedTet = Simplex(tetUsed.v)
+			for j in range(len(degreeslist)):
+				rotatedTet.Rotate(degreeslist[j],planelist[j],'deg')
+			# reject rotation if it results in a collision and scale back rotation size
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					if collision_detection(rotatedTet,updatedlist[j],subproc):
+						useRotation = False
+						rotranges[i] *= rotreduction
+						break
+				else:
+					if collision_detection(rotatedTet,tetralist[j],subproc):
+						useRotation = False
+						rotranges[i] *= rotreduction
+						break
 
+			# apply correct update
+			updatedTet = Simplex(tet.v)
+			if useTranslation:
+				updatedTet.translate(tvec)
+			if useRotation:
+				for j in range(len(degreeslist)):
+					updatedTet.Rotate(degreeslist[j],planelist[j],'deg')
+			updatedlist.append(updatedTet)
 
-#TODO: MC Search, (Maybe Gradient Descent)
+			# update center of mass
+			if i != len(tetralist)-1:
+				centerOfMass = getCOM(updatedlist+tetralist[i+1:])
+			else:
+				centerOfMass = getCOM(updatedlist)
+
+		# update tetralist, print density calculation, check convergence condition
+		tetralist = updatedlist[:]
+		if verbose and iteration % 10 == 9:
+			print("Iteration: " + str(iteration+1))
+			cubeVolume = cubeContainerVolume(tetralist)
+			print "box container volume: ", cubeVolume
+			tetVolume = l**3.0/(6*2**0.5)*numtetras
+			print "density: ", tetVolume/cubeVolume
+		if not False in converged:
+			break
+	return tetralist
+
+# Packing algorithm that randomly initializes numtetras tetrahedra in space
+# then compresses them by performing random translations and rotations that 
+# move the tetrahedra towards the collective "center of mass"
+# V3 adds local optimization bias to potentially reduce "dog piling" in center
+def randomizedGuidedPackingV3(numtetras, genmult=2, geninc=2, initstepscale=0.9, stepscalereduction=0.95, initrotrange=360.0, rotreduction=0.95, stepthreshold=1e-6, rotationthreshold=0.1, l=1.0, max_iters=1000, verbose=False):
+	assert(initstepscale>0 and initstepscale<=1)
+	assert(stepscalereduction>0 and stepscalereduction<1)
+	assert(stepthreshold>0)
+	assert(rotreduction>0 and rotreduction<1)
+
+	# Can be removed if python implementation of collision detection is used
+	subproc = subprocess.Popen('clojure', stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+	subproc.stdin.write('(clojure.main/load-script "tetrahedron-intersect.clj")')
+	subproc.stdin.write('(flush)')
+	subproc.stdout.readline() # flush out useless line
+	subproc.stdout.readline() # flush out useless line
+
+	tetralist = generate_tetrahedra(numtetras,genmult,geninc,l)
+	scales = [initstepscale]*len(tetralist)
+	rotranges = [initrotrange]*len(tetralist)
+	converged = [False]*len(tetralist)
+	centerOfMass = getCOM(tetralist)
+	for iteration in range(max_iters):
+		updatedlist = []
+		for i, tet in enumerate(tetralist):
+			# preliminary convergence check
+			useTranslation = True
+			useRotation = True
+			if scales[i] < stepthreshold and abs(rotranges[i])< rotationthreshold:
+				updatedlist.append(tet)
+				converged[i] = True
+				continue
+
+			# get translation
+			dist = np.linalg.norm(centerOfMass-tet.c)
+			step_size = scales[i]*dist
+			tvec = np.random.uniform(-step_size,step_size,len(tet.c))
+			# flip diretion of translation if it moves the tetrahedron away from COM
+			if np.linalg.norm(centerOfMass-tet.c-tvec)>dist:
+				tvec *= -1
+			# add local optimization bias
+			nearest = None
+			minDist = None
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					d = np.linalg.norm(updatedlist[j].c-tet.c)
+					if minDist == None or d<minDist:
+						minDist = d
+						nearest = updatedlist[j]
+				else:
+					d = np.linalg.norm(tetralist[j].c-tet.c)
+					if minDist == None or d<minDist:
+						minDist = d
+						nearest = tetralist[j]
+			# if only one tetrahedron
+			if nearest == None:
+				nearest = tet
+			bias = nearest.c - tet.c
+			for j in range(len(bias)):
+				tvec[j] += scales[i]*np.random.uniform(min(bias[j],0),max(bias[j],0))
+			translatedTet = Simplex(tet.v)
+			translatedTet.translate(tvec)
+			# reject translation if it results in a collision and scale back step size
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					if collision_detection(translatedTet,updatedlist[j],subproc):
+						useTranslation = False
+						scales[i] *= stepscalereduction
+						break
+				else:
+					if collision_detection(translatedTet,tetralist[j],subproc):
+						useTranslation = False
+						scales[i] *= stepscalereduction
+						break
+
+			tetUsed = tet
+			if useTranslation:
+				tetUsed = translatedTet
+			# fine nearest after possible translation
+			nearest = None
+			minDist = None
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					d = np.linalg.norm(updatedlist[j].c-tetUsed.c)
+					if minDist == None or d<minDist:
+						minDist = d
+						nearest = updatedlist[j]
+				else:
+					d = np.linalg.norm(tetralist[j].c-tetUsed.c)
+					if minDist == None or d<minDist:
+						minDist = d
+						nearest = tetralist[j]
+			# if only one tetrahedron
+			if nearest == None:
+				nearest = tetUsed
+
+			# get rotation
+			degreeslist = []
+			planelist = []
+			for j in range(len(tet.c)):
+				for k in range(j+1,len(tet.c)):
+					degrees = np.random.uniform(rotranges[i])
+					plane = (j,k)
+					rotatedTet = Simplex(tetUsed.v)
+					rotatedTet.Rotate(degrees,plane,'deg')
+					netdist1 = 0
+					netrdist1 = 0
+					netdist2 = 0
+					netrdist2 = 0
+					for n in range(len(tetUsed.v)):
+						d1 = np.linalg.norm(centerOfMass-tetUsed.v[n])
+						rd1 = np.linalg.norm(centerOfMass-rotatedTet.v[n])
+						d2 = np.linalg.norm(nearest.c-tetUsed.v[n])
+						rd2 = np.linalg.norm(nearest.c-rotatedTet.v[n])
+						netdist1 += d1
+						netrdist1 += rd1
+						netdist2 += d2
+						netrdist2 += rd2
+					# flip direction of rotation if it increases net vertex distance from COM
+					if netrdist1 > netdist1 and netrdist2 > netrdist2:
+						degrees *= -1
+					degreeslist.append(degrees)
+					planelist.append(plane)
+
+			rotatedTet = Simplex(tetUsed.v)
+			for j in range(len(degreeslist)):
+				rotatedTet.Rotate(degreeslist[j],planelist[j],'deg')
+			# reject rotation if it results in a collision and scale back rotation size
+			for j in range(len(tetralist)):
+				if j == i:
+					continue
+				elif j < i:
+					if collision_detection(rotatedTet,updatedlist[j],subproc):
+						useRotation = False
+						rotranges[i] *= rotreduction
+						break
+				else:
+					if collision_detection(rotatedTet,tetralist[j],subproc):
+						useRotation = False
+						rotranges[i] *= rotreduction
+						break
+
+			# apply correct update
+			updatedTet = Simplex(tet.v)
+			if useTranslation:
+				updatedTet.translate(tvec)
+			if useRotation:
+				for j in range(len(degreeslist)):
+					updatedTet.Rotate(degreeslist[j],planelist[j],'deg')
+			updatedlist.append(updatedTet)
+
+			# update center of mass
+			if i != len(tetralist)-1:
+				centerOfMass = getCOM(updatedlist+tetralist[i+1:])
+			else:
+				centerOfMass = getCOM(updatedlist)
+
+		# update tetralist, print density calculation, check convergence condition
+		tetralist = updatedlist[:]
+		if verbose and iteration % 10 == 9:
+			print("Iteration: " + str(iteration+1))
+			cubeVolume = cubeContainerVolume(tetralist)
+			print "box container volume: ", cubeVolume
+			tetVolume = l**3.0/(6*2**0.5)*numtetras
+			print "density: ", tetVolume/cubeVolume
+		if not False in converged:
+			break
+	return tetralist
